cff4d9d8fa105bbfd15f7314b8205132d0de3875
dopeycat <florienearp@gmail.com>
2026-01-29 17:02:04 +0000

    fixed undefined bug

--- main.js
+++ main.js
@@ -14,54 +14,56 @@ const randomInt = (max) => {
   return Math.floor(Math.random() * max);
 }
 
+const kinos = [{ai: [], number: 0}];
+
 export default {
-    kinos: [{ai: [], number: 0}],
+    kinos: kinos,
     init: (form) => {
         for(let i1 = 0; i1 < form.length; i1++) {
-            this.kinos[0].ai.push([]); // push layers
-            this.kinos[0].ai[i1].push([]); // push output array
+            kinos[0].ai.push([]); // push layers
+            kinos[0].ai[i1].push([]); // push output array
                 for(let i2 = 1; i2 < form[i1] + 1; i2++) {
                     if(i1 !== 0) {
-                        this.kinos[0].ai[i1].push({weights: [], bias: 0});
+                        kinos[0].ai[i1].push({weights: [], bias: 0});
                         for(let i3 = 0; i3 < form[i1 - 1]; i3++) {
-                            this.kinos[0].ai[i1][i2].weights.push(0);
+                            kinos[0].ai[i1][i2].weights.push(0);
                         }
                     } else {
-                        this.kinos[0].ai[i1].push(null);
+                        kinos[0].ai[i1].push(null);
                     }
                 }
         }
     },
     run: (input, kino) => {
-        this.kinos[0].ai[0][0] = input;
-        for(let i1 = 1; i1 < this.kinos[kino].ai.length; i1++) {
+        kinos[0].ai[0][0] = input;
+        for(let i1 = 1; i1 < kinos[kino].ai.length; i1++) {
 
-            for(let i2 = 1; i2 < this.kinos[kino].ai[i1].length; i2++) {
+            for(let i2 = 1; i2 < kinos[kino].ai[i1].length; i2++) {
                 
-                this.kinos[kino].ai[i1][0] = dot(this.kinos[kino].ai[i1 - 1][0], this.kinos[kino].ai[i1][i2].weights);
+                kinos[kino].ai[i1][0] = dot(kinos[kino].ai[i1 - 1][0], kinos[kino].ai[i1][i2].weights);
             }
         }
-        return this.kinos[kino].ai[this.kinos[kino].ai.length][0];
+        return kinos[kino].ai[kinos[kino].ai.length][0];
     },
     sortKinos: () => {
-        this.kinos.sort((a, b) => b.number - a.number);
+        kinos.sort((a, b) => b.number - a.number);
     },
     train: (split, intensity, unstability) => {
         this.sortKinos();
         // split kinos
         for(let i1 = 0; i1 < split - 1; i1++) {
             for(let i2 = 0; i2 < intensity; i2++) {
-                this.kinos.push(this.kinos[i1]);
+                kinos.push(kinos[i1]);
             }
         }
         // mutate kinos
         if(unstability != 0) {
-            for(let i1 = 0; i1 < this.kinos.length; i1++) {
+            for(let i1 = 0; i1 < kinos.length; i1++) {
                 for(let i1 = 0; i1 < unstability; i1++) {
-                    const r1 = randomInt(this.kinos[i1].ai.length);
-                    const r2 = randomInt(this.kinos[i1].ai[r1].length);
+                    const r1 = randomInt(kinos[i1].ai.length);
+                    const r2 = randomInt(kinos[i1].ai[r1].length);
                     const r3 = randomInt(2);
-                    const r4 = randomInt(this.kinos[i1].ai[r1].weights.length);
+                    const r4 = randomInt(kinos[i1].ai[r1].weights.length);
                     const r5 = randomInt(2);
                     let r6 = undefined;
 
@@ -72,13 +74,13 @@ export default {
                     }
 
                     if (r3 == 0) {
-                        this.kinos[i1].ai[r1][r2].weights[r4] += r6;
+                        kinos[i1].ai[r1][r2].weights[r4] += r6;
                     }
                 }
             }
         }
     },
     reward: (kino, amount) => {
-        this.kinos[kino].number += amount;
+        kinos[kino].number += amount;
     }
 }
