ddb0f82e3073fe1994bf6ad8d0b702615b4f821b
dopeycat <florienearp@gmail.com>
2026-02-01 13:40:21 +0000

    remade mutate in train

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
 // minidependencies
 
 const dot = (vectorA, vectorB) => {
-    if(!Array.isArray(vectorA) || !Array.isArray(vectorB)) {
-        throw new Error("Both inputs must be arrays");
-    }
-    if(vectorA.length !== vectorB.length) {
-        throw new Error("Vectors must be of the same length");
-    }
     return vectorA.reduce((acc, current, index) => {
         return acc + current * vectorB[index];
     }, 0);
 };
 
-const randomuint = (max) => {
+const rani = (max) => {
   return Math.round(Math.random() * max);
-};
-
-const randomFloat = (max) => {
-  return Math.random() * (max * 2) - max; // Generates a value between -max and max
 };
 
 // items in the library
 
 let kinos = [];
+
+let trainran = [0, 0, 0];
 
 const sortKinos = () => {
     kinos.sort((a, b) => b.number - a.number);
@@ -61,25 +53,29 @@ export default {
         }
         return kinos[kino].ai[kinos[kino].ai.length - 1][0];
     },
-    train: (split, unstability, change) => {
-        sortKinos();
+    train: (change, split, scale, unstability) => {
         // split kinos
+        sortKinos();
         for(let i1 = 0; i1 < split; i1++) {
             kinos.push(kinos[i1]);
         }
+
         // mutate kinos
-        let r1;
-        for(let i1 = 0; i1 < kinos.length; i1++) {
-            for(let i1 = 0; i1 < unstability; i1++) {
-                r1 = randomuint(kinos[i1].ai.length); // other random values use this value, cannot put into code directly
-                if(randomuint(1) == 0) {
-                    kinos[i1].ai[r1][randomuint(kinos[i1].ai[r1].length)].weights[randomuint(kinos[i1].ai[r1].weights.length)] += randomFloat(change);
+        for(let i1 = 0; i1 < unstability; i1++) {
+            for(let i2 = 0; i2 < kinos.length; i2++) {
+                trainran[0] = rani(kinos[i2].ai.length - 1);
+                trainran[1] = Math.max(1, rani(kinos[i2].ai[trainran[0].length - 1]));
+                trainran[2] = rani(kinos[i2].ai[trainran[0]][trainran[1]].weights.length - 1);
+                if(rani(scale) == 0) {
+                    kinos[i2].ai[trainran[0]][trainran[1]].bias += change;
                 } else {
-                    kinos[i1].ai[r1][randomuint(kinos[i1].ai[r1].length)].bias += randomFloat(change);
+                    kinos[i2].ai[trainran[0]][trainran[1]].weights[trainran[2]] += change;
                 }
+
             }
         }
 
+        // merge kinos
     },
     reward: (kino, amount) => {
         kinos[kino].number += amount;
