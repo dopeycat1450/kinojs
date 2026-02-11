3b6b1d28bcca6fbf493f92d95d6e2c3ff02516b4
dopeycat <florienearp@gmail.com>
2026-02-11 16:26:07 +0000

    changed kinos to ai (one ai)

--- main.js
+++ main.js
@@ -6,63 +6,54 @@ const dot = (vectorA, vectorB) => {
     }, 0);
 }
 
-const rani = (max) => {
-  return Math.round(Math.random() * max);
-}
-
 // items in the library
 
-let kinos = [];
+let ai = [];
 
-let t = {posdir: true, prevrewa: 0, layer: 1, neuron: 1, bweight: -1};
-
-const sortKinos = () => {
-    kinos.sort((a, b) => b.number - a.number);
-}
+let t = {posdir: true, rewa: 0, prevrewa: 0, layer: 1, neuron: 1, bweight: -1};
 
 export default {
-    kinos,
-    sortKinos,
+    kino,
+    sortkino,
     init: (form) => {
-        kinos.unshift({ai: [], number: 0});
         for(let i1 = 0; i1 < form.length; i1++) {
-            kinos[0].ai.push([]); // push layers
-            kinos[0].ai[i1].push([]); // push output array
+            ai.push([]); // push layers
+            ai[i1].push([]); // push output array
                 for(let i2 = 1; i2 < form[i1] + 1; i2++) {
-                    kinos[0].ai[i1].push({weights: [], bias: 0}); // push neurons
+                    ai[i1].push({weights: [], bias: 0}); // push neurons
                     for(let i3 = 0; i3 < form[i1 - 1]; i3++) {
-                        kinos[0].ai[i1][i2].weights.push(1); // push weights
+                        ai[i1][i2].weights.push(1); // push weights
                     }
                 }
         }
-        return kinos[0].ai;
+        return ai;
     },
     run: (input, kino) => {
-        kinos[0].ai[0][0] = input;
-        for(let i1 = 1; i1 < kinos[kino].ai.length; i1++) {
+        ai[0][0] = input;
+        for(let i1 = 1; i1 < kino[kino].ai.length; i1++) {
 
-            kinos[kino].ai[i1][0] = []; // reset layer output
-            for(let i2 = 1; i2 < kinos[kino].ai[i1].length; i2++) {
+            ai[i1][0] = []; // reset layer output
+            for(let i2 = 1; i2 < ai[i1].length; i2++) {
                 
-                kinos[kino].ai[i1][0].push(dot(kinos[kino].ai[i1 - 1][0], kinos[kino].ai[i1][i2].weights) + kinos[kino].ai[i1][i2].bias); // push neuron output to layer output
+                ai[i1][0].push(dot(ai[i1 - 1][0], ai[i1][i2].weights) + ai[i1][i2].bias); // push neuron output to layer output
             }
         }
-        return kinos[kino].ai[kinos[kino].ai.length - 1][0];
+        return ai[ai.length - 1][0];
     },
-    singleTrain: (kino, amount) => {
+    singleTrain: (amount) => {
+        let weigh;
         if(t.posdir == true) {
-            let weigh = amount;
+            weigh = amount;
         } else {
-            let weigh = 0 - amount;
+            weigh = 0 - amount;
         }
         if(t.bweight = -1) {
-            kinos[kino].ai[t.layer][t.neuron].bias += weigh;
+            ai[t.layer][t.neuron].bias += weigh;
         } else {
-            kinos[kino].ai[t.layer][t.neuron].weights[t.bweight] += weigh;
+            ai[t.layer][t.neuron].weights[t.bweight] += weigh;
         }
     },
-    reward: (kino, amount) => {
-        t.prevrewa = amount;
-        kinos[kino].number += amount;
+    reward: (amount) => {
+        t.rewa = amount;
     }
 };
